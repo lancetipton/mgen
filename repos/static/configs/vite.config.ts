40d10f920de7d4d35f913444f72bcbefa1ebb5f6
Lance Tipton <lance.tipton@i-360.com>
2024-12-18 15:30:09 -0700

    feat: remove static

--- repos/static/configs/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import type { UserConfig } from 'vite'
-
-import { defineConfig } from 'vite'
-import { config } from './vite.workspace'
-
-export default defineConfig(config as UserConfig)
--- repos/static/scripts/registerPaths.ts
+++
@@ -1,8 +0,0 @@
-import 'esbuild-register'
-
-import tsConfig from '../tsconfig.json'
-import { register } from 'tsconfig-paths'
-register({
-  baseUrl: '../../../',
-  paths: tsConfig.compilerOptions.paths,
-})
